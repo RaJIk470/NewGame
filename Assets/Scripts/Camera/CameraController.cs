d4d00cd662a16ab3109af647196cf5d2d7a378c0
RaJIk470 <67538138+RaJIk470@users.noreply.github.com>
2020-12-16 16:28:17 +0300

    Initial commit

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+    Transform _transform;
+    
+    void Start()
+    {
+        _transform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        _transform.position = new Vector3(Player.position.x, Player.position.y, _transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpForce = 5;
+    Rigidbody2D _rigidbody;
+    Transform _transform;
+    Animator _animator;
+    Vector2 _movement;
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    void FixedUpdate()   
+    {
+        _movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Magnitude", _movement.magnitude);
+        _rigidbody.velocity = _movement;
+    }
+
+}
